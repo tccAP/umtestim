12bdbf639e4a6a1b3b70ba3760c99db938b2afb0
Paulo <paulim0015@hotmail.com>
2023-01-15 07:27:35 -0300

    2

--- src/main/java/com/example/umtestim/demo/controller/TesteController.java
+++ src/main/java/com/example/umtestim/demo/controller/TesteController.java
@@ -1,10 +1,15 @@
 package com.example.umtestim.demo.controller;
 
+import com.example.umtestim.demo.service.TesteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TesteController {
+
+    @Autowired
+    private TesteService service;
 
     @GetMapping("/teste")
     public String teste(){
@@ -36,6 +41,8 @@ public class TesteController {
         System.out.println("djkasdkasodkasod");
         System.out.println("djkasdkasodkasod");
         System.out.println("djkasdkasodkasod");
+        System.out.println(service.teste());
+
 
         if(1==2){
 
---
+++ src/main/java/com/example/umtestim/demo/service/TesteService.java
@@ -0,0 +1,12 @@
+package com.example.umtestim.demo.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TesteService {
+
+    public String teste(){
+        return "service";
+    }
+}
